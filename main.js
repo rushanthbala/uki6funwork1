e087c3ff480ddbcf5b473f57e8c278bac1321cfe
rushanth123 <58292966+rushanth123@users.noreply.github.com>
2020-04-25 00:14:04 +0530

    Update main.js

--- main.js
+++ main.js
@@ -204,6 +204,102 @@ $(document).ready(function() {
                    sound.stop();
                 });
           });
+  
+  
+    SC.stream('/tracks/95987913',function(sound){
+                     $('#start-f5').click(function(e) {
+                            e.preventDefault();
+                            sound.start();
+                          });
+                     $('#stop-f5').click(function(e) {
+                            e.preventDefault();
+                            sound.stop();
+                         });
+                   });
+     //<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/559876704&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/user-749183989" title="Hiba" target="_blank" style="color: #cccccc; text-decoration: none;">Hiba</a> · <a href="https://soundcloud.com/user-749183989/lakshmi_morrakka_tamil_song_vi" title="lakshmi_morrakka_tamil_song_video_prabhu_deva_aishwarya_rajesh_ditya_vijay_sam_cs_h264_50428_1544450340281_a01.mp3" target="_blank" style="color: #cccccc; text-decoration: none;">lakshmi_morrakka_tamil_song_video_prabhu_deva_aishwarya_rajesh_ditya_vijay_sam_cs_h264_50428_1544450340281_a01.mp3</a></div>
+                   SC.stream('/tracks/749183989',function(sound){
+                     $('#start-f6').click(function(e) {
+                            e.preventDefault();
+                            sound.start();
+                          });
+                     $('#stop-f6').click(function(e) {
+                            e.preventDefault();
+                            sound.stop();
+                         });
+                   });
+
+     //<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/17864018&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/mjtheboss" title="Mjtheboss" target="_blank" style="color: #cccccc; text-decoration: none;">Mjtheboss</a> · <a href="https://soundcloud.com/mjtheboss/makkayala-makkayala-naan-vijay" title="Makkayala Makkayala - Naan (Vijay Antony) Single Track" target="_blank" style="color: #cccccc; text-decoration: none;">Makkayala Makkayala - Naan (Vijay Antony) Single Track</a></div>
+                   SC.stream('/tracks/17864018',function(sound){
+                     $('#start-f7').click(function(e) {
+                            e.preventDefault();
+                            sound.start();
+                          });
+                     $('#stop-f7').click(function(e) {
+                            e.preventDefault();
+                            sound.stop();
+                         });
+                   });
+     
+{/* <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/153257092&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/poet-14" title="Pottuvil Asmin (Lyricist)" target="_blank" style="color: #cccccc; text-decoration: none;">Pottuvil Asmin (Lyricist)</a> · <a href="https://soundcloud.com/poet-14/thapellam-thape-illai" title="Thapellam Thape Illai" target="_blank" style="color: #cccccc; text-decoration: none;">Thapellam Thape Illai</a></div> */}
+
+                   SC.stream('/tracks/153257092',function(sound){
+                     $('#start-f8').click(function(e) {
+                            e.preventDefault();
+                            sound.start();
+                          });
+                     $('#stop-f8').click(function(e) {
+                            e.preventDefault();
+                            sound.stop();
+                         });
+                   });
+//<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/270886186&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/iamansal" title="Ansal" target="_blank" style="color: #cccccc; text-decoration: none;">Ansal</a> · <a href="https://soundcloud.com/iamansal/takkarutakkaru" title="Takkaru Takkaru | HipHop Tamizha Aadhi  | iTunes | Ansal" target="_blank" style="color: #cccccc; text-decoration: none;">Takkaru Takkaru | HipHop Tamizha Aadhi  | iTunes | Ansal</a></div>
+                   
+
+                   SC.stream('/tracks/270886186',function(sound){
+                     $('#start-f9').click(function(e) {
+                            e.preventDefault();
+                            sound.start();
+                          });
+                     $('#stop-f9').click(function(e) {
+                            e.preventDefault();
+                            sound.stop();
+                         });
+                   });
+//<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/102921318&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/ramprasad-1" title="Ram Prasad" target="_blank" style="color: #cccccc; text-decoration: none;">Ram Prasad</a> · <a href="https://soundcloud.com/ramprasad-1/virumandi-onnavida-www" title="Virumandi - Onnavida - Www.TamilKey.Com" target="_blank" style="color: #cccccc; text-decoration: none;">Virumandi - Onnavida - Www.TamilKey.Com</a></div>     
+
+                   SC.stream('/tracks/102921318',function(sound){
+                     $('#start-f10').click(function(e) {
+                            e.preventDefault();
+                            sound.start();
+                          });
+                     $('#stop-f10').click(function(e) {
+                            e.preventDefault();
+                            sound.stop();
+                         });
+                   });
+     //<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/156118358&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/kumarappankumar" title="Kumarappan Kumar" target="_blank" style="color: #cccccc; text-decoration: none;">Kumarappan Kumar</a> · <a href="https://soundcloud.com/kumarappankumar/tamil-folk-beats-flim-pokkiri-2006" title="Tamil Folk Beats - Flim: Pokkiri (2006)" target="_blank" style="color: #cccccc; text-decoration: none;">Tamil Folk Beats - Flim: Pokkiri (2006)</a></div>
+                   SC.stream('/tracks/156118358',function(sound){
+                     $('#start-f11').click(function(e) {
+                            e.preventDefault();
+                            sound.start();
+                          });
+                     $('#stop-f11').click(function(e) {
+                            e.preventDefault();
+                            sound.stop();
+                         });
+                   });
+     //<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/211984914&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/songspkfit-indian-music-collection" title="SongsPKFit Collection" target="_blank" style="color: #cccccc; text-decoration: none;">SongsPKFit Collection</a> · <a href="https://soundcloud.com/songspkfit-indian-music-collection/sivuni-aana-wwwsongspkfit" title="Sivuni Aana [www.songspk.fit]" target="_blank" style="color: #cccccc; text-decoration: none;">Sivuni Aana [www.songspk.fit]</a></div>
+                   SC.stream('/tracks/211984914',function(sound){
+                     $('#start-f12').click(function(e) {
+                            e.preventDefault();
+                            sound.start();
+                          });
+                     $('#stop-f12').click(function(e) {
+                            e.preventDefault();
+                            sound.stop();
+                         });
+                   });
+     
 
 
 });
