fa4deac22a097890b4827939546306326e60ad59
rushanth123 <58292966+rushanth123@users.noreply.github.com>
2020-05-02 19:06:30 +0530

    Update main.js
    finish 3 part

--- main.js
+++ main.js
@@ -42,6 +42,165 @@ $(document).ready(function() {
         });
   });
   
+  
+ 
+// {/* <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/569398809&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/srinivas-koda" title="Srinivas Koda" target="_blank" style="color: #cccccc; text-decoration: none;">Srinivas Koda</a> · <a href="https://soundcloud.com/srinivas-koda/petta-bgm" title="Petta - BGM" target="_blank" style="color: #cccccc; text-decoration: none;">Petta - BGM</a></div> */}
+  SC.stream('/tracks/569398809',function(sound){
+       $('#start-r4').click(function(e) {
+              e.preventDefault();
+              sound.start();
+            });
+       $('#stop-r4').click(function(e) {
+              e.preventDefault();
+              sound.stop();
+           });
+     });
+     
+   
+//      <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/283781104&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/sriramk152" title="sriramk152" target="_blank" style="color: #cccccc; text-decoration: none;">sriramk152</a> · <a href="https://soundcloud.com/sriramk152/saithan-theme-vijay-antony-arundhathi-nair-pradeep-krishnamoorthy" title="Saithan - Theme - Vijay Antony, Arundhathi Nair - Pradeep Krishnamoorthy" target="_blank" style="color: #cccccc; text-decoration: none;">Saithan - Theme - Vijay Antony, Arundhathi Nair - Pradeep Krishnamoorthy</a></div>
+     SC.stream('/tracks/283781104',function(sound){
+       $('#start-r5').click(function(e) {
+              e.preventDefault();
+              sound.start();
+            });
+       $('#stop-r5').click(function(e) {
+              e.preventDefault();
+              sound.stop();
+           });
+     });
+     
+//    <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/37770685&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/ashwincinemass" title="Ashwin Cinemass" target="_blank" style="color: #cccccc; text-decoration: none;">Ashwin Cinemass</a> · <a href="https://soundcloud.com/ashwincinemass/rathiri-nerathu-poojayil-remix" title="Rathiri Nerathu Poojayil Remix - Vijay Antony HITZ" target="_blank" style="color: #cccccc; text-decoration: none;">Rathiri Nerathu Poojayil Remix - Vijay Antony HITZ</a></div>
+     SC.stream('/tracks/37770685',function(sound){
+       $('#start-r6').click(function(e) {
+              e.preventDefault();
+              sound.start();
+            });
+       $('#stop-r6').click(function(e) {
+              e.preventDefault();
+              sound.stop();
+           });
+     });
+     
+   
+// {/* <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/14841257&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/kadhalviruz" title="Kadhalviruz" target="_blank" style="color: #cccccc; text-decoration: none;">Kadhalviruz</a> · <a href="https://soundcloud.com/kadhalviruz/stolen-my-heart" title="Stolen My Heart" target="_blank" style="color: #cccccc; text-decoration: none;">Stolen My Heart</a></div> */}
+     SC.stream('/tracks/14841257',function(sound){
+       $('#start-r7').click(function(e) {
+              e.preventDefault();
+              sound.start();
+            });
+       $('#stop-r7').click(function(e) {
+              e.preventDefault();
+              sound.stop();
+           });
+     });
+     
+   
+// {/* <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/335930003&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/deyocdj" title="DJ DEYO" target="_blank" style="color: #cccccc; text-decoration: none;">DJ DEYO</a> · <a href="https://soundcloud.com/deyocdj/surviva-dj-deyo-remix-vivekam-anirudh-ravichander-ft-yogi-b" title="Surviva (DJ Deyo Remix) Vivekam - Anirudh Ravichander Ft Yogi B" target="_blank" style="color: #cccccc; text-decoration: none;">Surviva (DJ Deyo Remix) Vivekam - Anirudh Ravichander Ft Yogi B</a></div> */}
+     SC.stream('/tracks/335930003',function(sound){
+       $('#start-r8').click(function(e) {
+              e.preventDefault();
+              sound.start();
+            });
+       $('#stop-r8').click(function(e) {
+              e.preventDefault();
+              sound.stop();
+           });
+     });
+     
+   
+// {/* <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/14842523&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/kadhalviruz" title="Kadhalviruz" target="_blank" style="color: #cccccc; text-decoration: none;">Kadhalviruz</a> · <a href="https://soundcloud.com/kadhalviruz/chittu-kuruvi" title="Chittu Kuruvi" target="_blank" style="color: #cccccc; text-decoration: none;">Chittu Kuruvi</a></div> */}
+     SC.stream('/tracks/14842523',function(sound){
+       $('#start-r9').click(function(e) {
+              e.preventDefault();
+              sound.start();
+            });
+       $('#stop-r9').click(function(e) {
+              e.preventDefault();
+              sound.stop();
+           });
+     });
+     
+   
+// {/* <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/254948580&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/insanegowtham" title="Gowtham" target="_blank" style="color: #cccccc; text-decoration: none;">Gowtham</a> · <a href="https://soundcloud.com/insanegowtham/theri-theme-dub-step-dub-theri-step-theri-tamil-movie-gv-prakash-kumar" title="Theri Theme Dub Step (Dub Theri Step) - Theri Tamil Movie | GV Prakash Kumar" target="_blank" style="color: #cccccc; text-decoration: none;">Theri Theme Dub Step (Dub Theri Step) - Theri Tamil Movie | GV Prakash Kumar</a></div>  */}
+  SC.stream('/tracks/254948580',function(sound){
+       $('#start-r10').click(function(e) {
+              e.preventDefault();
+              sound.start();
+            });
+       $('#stop-r10').click(function(e) {
+              e.preventDefault();
+              sound.stop();
+           });
+     });
+     
+   
+// <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/57748075&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/sivasham" title="sivasham" target="_blank" style="color: #cccccc; text-decoration: none;">sivasham</a> · <a href="https://soundcloud.com/sivasham/hiphop-tamizha-must-to-watch" title="Hiphop Tamizha - Must to Watch All Tamil Girls" target="_blank" style="color: #cccccc; text-decoration: none;">Hiphop Tamizha - Must to Watch All Tamil Girls</a></div>
+     SC.stream('/tracks/57748075',function(sound){
+       $('#start-r11').click(function(e) {
+              e.preventDefault();
+              sound.start();
+            });
+       $('#stop-r11').click(function(e) {
+              e.preventDefault();
+              sound.stop();
+           });
+     });
+     
+   
+// <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/169941729&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/mc-starboy" title="MC Starboy Official" target="_blank" style="color: #cccccc; text-decoration: none;">MC Starboy Official</a> · <a href="https://soundcloud.com/mc-starboy/tamil-mc-starboy-ft-tattoo-the-rapper-prod-by-vajen-j-official-audio" title="Tamil - MC Starboy Ft. Tattoo The Rapper Prod by Vajen J (Official Audio )" target="_blank" style="color: #cccccc; text-decoration: none;">Tamil - MC Starboy Ft. Tattoo The Rapper Prod by Vajen J (Official Audio )</a></div>
+     SC.stream('/tracks/169941729',function(sound){
+       $('#start-r12').click(function(e) {
+              e.preventDefault();
+              sound.start();
+            });
+       $('#stop-r12').click(function(e) {
+              e.preventDefault();
+              sound.stop();
+           });
+     });
+     
+   
+// <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/81805142&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/thinesh_mangalanathan" title="thinesh_mangalanathan" target="_blank" style="color: #cccccc; text-decoration: none;">thinesh_mangalanathan</a> · <a href="https://soundcloud.com/thinesh_mangalanathan/t-va-ft-o-mama" title="T-va ft.  - O MAMA  .. tamil rap 2013" target="_blank" style="color: #cccccc; text-decoration: none;">T-va ft.  - O MAMA  .. tamil rap 2013</a></div>
+     SC.stream('/tracks/81805142',function(sound){
+       $('#start-r13').click(function(e) {
+              e.preventDefault();
+              sound.start();
+            });
+       $('#stop-r13').click(function(e) {
+              e.preventDefault();
+              sound.stop();
+           });
+     });
+     
+   
+// <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/211453128&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/musicbyrahul" title="R3HTOM" target="_blank" style="color: #cccccc; text-decoration: none;">R3HTOM</a> · <a href="https://soundcloud.com/musicbyrahul/donu-donu-trap-remix-maari-tamil-trap-mix" title="Don'u Don'u Don'u Trap remix Maari Tamil Trap Mix Rahul Sharma (R3HTOM Remix)" target="_blank" style="color: #cccccc; text-decoration: none;">Don'u Don'u Don'u Trap remix Maari Tamil Trap Mix Rahul Sharma (R3HTOM Remix)</a></div>
+     SC.stream('/tracks/211453128',function(sound){
+       $('#start-r14').click(function(e) {
+              e.preventDefault();
+              sound.start();
+            });
+       $('#stop-r14').click(function(e) {
+              e.preventDefault();
+              sound.stop();
+           });
+     });
+     
+   
+// <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/244423685&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/sugi-rajah" title="SugiRajah" target="_blank" style="color: #cccccc; text-decoration: none;">SugiRajah</a> · <a href="https://soundcloud.com/sugi-rajah/vidai-kodai-engal-naade-by-saranmohan-itsemarv-and-sugirajah" title="Vidai Kodu - by SaranMohan, Itsemarv and SugiRajah" target="_blank" style="color: #cccccc; text-decoration: none;">Vidai Kodu - by SaranMohan, Itsemarv and SugiRajah</a></div>
+     SC.stream('/tracks/244423685',function(sound){
+       $('#start-r15').click(function(e) {
+              e.preventDefault();
+              sound.start();
+            });
+       $('#stop-r15').click(function(e) {
+              e.preventDefault();
+              sound.stop();
+           });
+     });
+     
+   
+   
+  
 
 //   <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay"
 //    src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/108633175&color=%23ff5500&auto_play=true&hid
